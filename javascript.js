75a6b6ecda4880a077e4e884442cfe2184de5814
MrDamian1723 <99835567+MrDamian1723@users.noreply.github.com>
2022-02-22 20:45:12 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,24 @@
+
+function cambiarDimensiones()
+{
+	imagen.style="height:50%;width:50%;";
+	imagen2.style="height:50%;width:50%;";
+}
+
+function restaurarDimensiones()
+{
+	imagen.style="";
+	imagen2.style="";
+}
+
+function cambiarImagen()
+{	
+	imagen3.style="height:50%;width:50%;"
+	imagen3.src="https://blogger.googleusercontent.com/img/a/AVvXsEiEP3didh2aTdDYh6bAWss-bCHDHErJmyXNdAF0CNz13ByGRQrca24PSnD4vMPgimiYJ1_XFwMhxMiDlLCeJpuPb2_7sTZzjhCqExCaFGJ20UXAjPVfWVgh_IcX_ey6PKevATXqF4DikCav9MHsQmWS6xqsWzD6vgEh1m7C1KwXk7AIe1KnvqMEU3Xk=w521-h101"
+}
+
+function volverImagen()
+{
+	imagen3.style="";
+	imagen3.src="https://blogger.googleusercontent.com/img/a/AVvXsEjnh6LdhuvKtDWFfZmJvIXOxBoWVo_AEBJum5lDcqBrnLtut5-p-ycProAWKCKScQGGNsDUExfyBEQteN9wzDfCzsHhXfqUyWy6cuvH_yn9VOIpy7urMH-dxypgwDD1HqXM0GhQLlu7Fbw3s6JEpS_2nWZrmmTQZWGF67u5QQwbNSnWrBqVgZwX_fZf";
+}
